65957c122ef99ea5241a1ce27a8ed58e57320b41
Enol Vallina <enol.vallina@outlook.com>
2016-11-15 18:23:21 -0500

    Development

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 
-
-$.getJSON("http://api.songkick.com/api/3.0/events.json?location=clientip&apikey={ME5jCBPTyD3l4BW8}&jsoncallback=?",
+$.getJSON("http://api.songkick.com/api/3.0/events.json?location=clientip&apikey=ME5jCBPTyD3l4BW8&jsoncallback=?",
     function(data){
-        console.log(data);
+        createVisualization(data);
+
+
 // data is JSON response object
     });
+//
+// data.resultsPage.results.event
+// data["resultsPage"]["results"]["event"]
+
+function createVisualization(data) {
+    console.log(data);
+    var events  = data["resultsPage"]["results"]["event"];
+    for (var i = 0; i < events.length; i++) {
+        $("#data").append("<p>" + events[i]["displayName"] + "</p>");
+        $("#data").append("<p><b>Popularity: </b>" + events[i]["popularity"] + "</p>");
+    }
+}
